b310c802d5d918471cb298c68bbdbd7e3b5dd63a
sedrickGerards <mubangsedrick@gmail.com>
2025-10-08 15:33:23 +0100

    setting up  tailwind for the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="bg-blue-50">
       <h1>Hello World!</h1>
 
       <p>Welcome to scholar </p>
-    </>
+    </div>
   );
 }
 
