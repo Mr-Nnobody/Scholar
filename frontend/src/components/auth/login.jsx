2d771848c395d5b4814ecb9645141ccefee7d8ac
sedrickGerards <mubangsedrick@gmail.com>
2025-10-16 21:26:02 +0100

    finished handling login submission

--- frontend/src/components/auth/login.jsx
+++ frontend/src/components/auth/login.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 export default function Login() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleEventLogin = (e) => {
+    e.preventDefault();
 
+    console.log(`the login name ${name} and the password ${password}`);
+  };
 
   return (
     <div class="flex justify-center items-center h-screen">
@@ -13,12 +18,15 @@ export default function Login() {
           <span class="text-red-500 font-bold"> Scholarship</span>
         </p>
         <form class="flex flex-col w-5/6 mx-auto  ">
-      
           <input
             name="userName"
             type="text"
             required
             autoComplete="userName"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
             class="my-2 p-2 rounded focus:outline-none focus:ring-sky-200 focus:ring-2 pl-4 text-lg"
             placeholder="Enter Your Name"
           />
@@ -27,12 +35,21 @@ export default function Login() {
             type="password"
             name="password"
             required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             class="my-2 p-2 rounded focus:outline-none focus:ring-sky-200 focus:ring-2 pl-4 text-lg"
             placeholder=" Enter Your Password"
           />
 
           <div class=" w-full  my-8  px-4 text-xl ">
-            <button type="submit" class="bg-green-400 rounded px-4 py-1">
+            <button
+              type="submit"
+              class="bg-green-400 rounded px-4 py-1"
+
+              onClick={handleEventLogin}
+            >
               Login
             </button>
           </div>
