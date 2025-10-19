5b8d6e8bb8ace4528e7250a74d16e17693e04004
sedrickGerards <mubangsedrick@gmail.com>
2025-10-19 23:56:37 +0100

    inputs for users

--- frontend/src/components/homepage/home.jsx
+++ frontend/src/components/homepage/home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-export default function Home(){
-
+export default function Home() {
   return (
     <div>
       <div>
@@ -11,13 +10,20 @@ export default function Home(){
             Simplify and focus your application process with the one-stop
             platform for vetted scholarships.{" "}
           </p>
-
-          <form action="submit">
-            <label htmlFor="DOB">Date of Birth</label>
-            <input type="month" />
-          </form>
+          
+            <form class="flex flex-col">
+              <label htmlFor="DOB">Date of Birth</label>
+              <input type="month" placeholder="select your birth month" />
+              <label htmlFor="Current school level">Current school level</label>
+              <input type="text" name="" id="education" />
+              <label htmlFor="Field of study">Field of study</label>
+              <input type="text" placeholder="Field of study " />
+            </form>
+         
         </div>
-        <div></div>
+        <div>
+          <img src="../assets/images/graduating.jpg" alt="graduate photo" width="200px" />
+        </div>
       </div>
       <div> banner</div>
     </div>
