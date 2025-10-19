2247eef553d823bed1c92d99fcbd7cab06e22a90
sedrickGerards <mubangsedrick@gmail.com>
2025-10-19 23:10:15 +0100

    created HOme page file included the route as Index and also hero image

---
+++ frontend/src/components/homepage/home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Home(){
+
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Win more scholarships with less effort </p>
+          <p>
+            Simplify and focus your application process with the one-stop
+            platform for vetted scholarships.{" "}
+          </p>
+
+          <form action="submit">
+            <label htmlFor="DOB">Date of Birth</label>
+            <input type="month" />
+          </form>
+        </div>
+        <div></div>
+      </div>
+      <div> banner</div>
+    </div>
+  );
+}
